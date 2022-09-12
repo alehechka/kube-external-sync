9fb0d1b0719dbd3dea076dd97cc0d0add40cd360
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-09-11 22:03:24 -0500

    remove secret/namespace loggers

--- client/loggers.go
+++ client/loggers.go
@@ -3,17 +3,8 @@ package client
 import (
 	typesv1 "github.com/alehechka/kube-external-sync/api/types/v1"
 	log "github.com/sirupsen/logrus"
-	v1 "k8s.io/api/core/v1"
 )
 
 func ruleLogger(rule *typesv1.ExternalSyncRule) *log.Entry {
 	return log.WithFields(log.Fields{"name": rule.Name, "kind": "ExternalSyncRule"})
-}
-
-func secretLogger(secret *v1.Secret) *log.Entry {
-	return log.WithFields(log.Fields{"name": secret.Name, "kind": "Secret", "namespace": secret.Namespace})
-}
-
-func namespaceLogger(namespace *v1.Namespace) *log.Entry {
-	return log.WithFields(log.Fields{"name": namespace.Name, "kind": "Namespace"})
 }
