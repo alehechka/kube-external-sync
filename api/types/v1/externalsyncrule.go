efcd6adc842a82233ded707ebe8d2cef174e90c8
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-09-12 21:00:49 -0500

    validate Namespace for rule(s)

--- api/types/v1/externalsyncrule.go
+++ api/types/v1/externalsyncrule.go
@@ -1,8 +1,12 @@
 package v1
 
 import (
+	"context"
+
 	"github.com/alehechka/kube-external-sync/api/types"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 // +kubebuilder:object:root=true
@@ -87,4 +91,54 @@ func (rule *ExternalSyncRule) HasService() bool {
 func (rule *ExternalSyncRule) HasIngress() bool {
 	return rule.Spec.Ingress != nil &&
 		len(rule.Spec.Ingress.Name) > 0
+}
+
+// ShouldSyncNamespace determines whether or not the given Namespace should be synced
+func (rule *ExternalSyncRule) ShouldSyncNamespace(namespace *v1.Namespace) bool {
+	rules := rule.Spec.Rules
+
+	if rule.Spec.Namespace == namespace.Name {
+		return false
+	}
+
+	if rules.Namespaces.Exclude.IsExcluded(namespace.Name) || rules.Namespaces.ExcludeRegex.IsRegexExcluded(namespace.Name) {
+		return false
+	}
+
+	if rules.Namespaces.Include.IsEmpty() && rules.Namespaces.IncludeRegex.IsEmpty() && rules.Namespaces.IncludeAnnotation.IsEmpty() {
+		return true
+	}
+
+	if rules.Namespaces.Include.IsIncluded(namespace.Name) || rules.Namespaces.IncludeRegex.IsRegexIncluded(namespace.Name) || rules.Namespaces.IncludeAnnotation.AnnotatesNamespace(namespace) {
+		return true
+	}
+
+	return false
+}
+
+// Namespaces returns a list of all namespaces that the given Rule allows for syncing
+func (rule *ExternalSyncRule) Namespaces(ctx context.Context, clientset kubernetes.Interface) (namespaces []v1.Namespace) {
+	list, err := clientset.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return
+	}
+
+	for _, namespace := range list.Items {
+		if rule.ShouldSyncNamespace(&namespace) {
+			namespaces = append(namespaces, namespace)
+		}
+	}
+
+	return
+}
+
+// ShouldSyncNamespace iterates over the list to determine whether or not the given Namespace should be synced
+func (list *ExternalSyncRuleList) ShouldSyncNamespace(namespace *v1.Namespace) bool {
+	for _, rule := range list.Items {
+		if rule.ShouldSyncNamespace(namespace) {
+			return true
+		}
+	}
+
+	return false
 }
