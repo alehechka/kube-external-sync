42215205270264df129553ac63a364d31ab77b3c
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-09-13 20:25:31 -0500

    ModifiedExternalSyncRuleHandler

--- client/externalsyncrules.go
+++ client/externalsyncrules.go
@@ -18,7 +18,7 @@ func (client *Client) ExternalSyncRuleEventHandler(event watch.Event) error {
 	case watch.Added:
 		return client.AddedExternalSyncRuleHandler(rule)
 	case watch.Modified:
-		ruleLogger(rule).Infof("modified: %#v", rule)
+		return client.ModifiedExternalSyncRuleHandler(rule)
 	case watch.Deleted:
 		ruleLogger(rule).Infof("deleted: %#v", rule)
 	}
@@ -28,6 +28,32 @@ func (client *Client) ExternalSyncRuleEventHandler(event watch.Event) error {
 
 func (client *Client) AddedExternalSyncRuleHandler(rule *typesv1.ExternalSyncRule) (err error) {
 	ruleLogger(rule).Infof("added")
+
+	var service *v1.Service = nil
+	if rule.HasService() && rule.Spec.Service.IsService() {
+		service, _ = client.GetService(rule.Spec.Namespace, rule.Spec.Service.Name)
+	}
+
+	for _, namespace := range rule.Namespaces(client.Context, client.DefaultClientset) {
+		if service != nil {
+			client.CreateUpdateExternalNameService(rule, &namespace, service)
+		}
+	}
+
+	return nil
+}
+
+// ModifiedExternalSyncRuleHandler handles syncing Services/Ingresses after a ExternalSyncRule has been modified
+//
+// Due to the event watcher only providing the new state of the modified resource, it is impossible to know the previous state.
+// (The exception to this is potentially "applied" changes and parsing the last-applied-configuration annotation)
+// In coping with this limitation, a modified ExternalSyncRule will simply attempt to resync the rule across all applicable namespaces.
+func (client *Client) ModifiedExternalSyncRuleHandler(rule *typesv1.ExternalSyncRule) error {
+	if rule.DeletionTimestamp != nil {
+		return nil
+	}
+
+	ruleLogger(rule).Infof("modified")
 
 	var service *v1.Service = nil
 	if rule.HasService() && rule.Spec.Service.IsService() {
