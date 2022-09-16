9e2b6d6861338191d82923c9dc3e11680bc13f92
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-09-15 20:13:00 -0500

    CRUD operations for Ingresses

--- client/ingresses.go
+++ client/ingresses.go
@@ -1,9 +1,12 @@
 package client
 
 import (
+	typesv1 "github.com/alehechka/kube-external-sync/api/types/v1"
 	"github.com/alehechka/kube-external-sync/constants"
 	log "github.com/sirupsen/logrus"
+	v1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
 )
 
@@ -31,6 +34,74 @@ func (client *Client) IngressEventHandler(event watch.Event) error {
 	}
 
 	return nil
+}
+
+func (client *Client) GetIngress(namespace, name string) (ingress *networkingv1.Ingress, err error) {
+	ingress, err = client.DefaultClientset.NetworkingV1().Ingresses(namespace).Get(client.Context, name, metav1.GetOptions{})
+	if err != nil {
+		ingressNameLogger(namespace, name).
+			Errorf("failed to get ingress: %s", err.Error())
+	}
+	return
+}
+
+func (client *Client) ListIngresses(namespace string) (list *networkingv1.IngressList, err error) {
+	list, err = client.DefaultClientset.NetworkingV1().Ingresses(namespace).List(client.Context, metav1.ListOptions{})
+	if err != nil {
+		namespaceNameLogger(namespace).
+			Errorf("failed to list ingresses: %s", err.Error())
+	}
+	return
+}
+
+func (client *Client) CreateIngress(rule *typesv1.ExternalSyncRule, namespace *v1.Namespace, ingress *networkingv1.Ingress) error {
+	newIngress := PrepareIngress(rule, namespace, ingress)
+
+	logger := ingressLogger(newIngress)
+	logger.Infof("creating")
+
+	_, err := client.DefaultClientset.NetworkingV1().Ingresses(namespace.Name).Create(client.Context, newIngress, metav1.CreateOptions{})
+
+	if err != nil {
+		logger.Errorf("failed to create ingress - %s", err.Error())
+	}
+
+	return err
+}
+
+func (client *Client) UpdateIngress(rule *typesv1.ExternalSyncRule, namespace *v1.Namespace, ingress *networkingv1.Ingress) error {
+	updateIngress := PrepareIngress(rule, namespace, ingress)
+
+	logger := ingressLogger(updateIngress)
+	logger.Infof("updating")
+
+	_, err := client.DefaultClientset.NetworkingV1().Ingresses(namespace.Name).Update(client.Context, updateIngress, metav1.UpdateOptions{})
+
+	if err != nil {
+		logger.Errorf("failed to update service - %s", err.Error())
+	}
+
+	return err
+}
+
+func (client *Client) DeleteIngress(namespace *v1.Namespace, ingress *networkingv1.Ingress) (err error) {
+	logger := ingressLogger(ingress, namespace)
+
+	logger.Infof("deleting")
+
+	err = client.DefaultClientset.NetworkingV1().Ingresses(namespace.Name).Delete(client.Context, ingress.Name, metav1.DeleteOptions{})
+	if err != nil {
+		logger.Errorf("failed to delete ingress - %s", err.Error())
+	}
+
+	return
+}
+
+func PrepareIngress(rule *typesv1.ExternalSyncRule, namespace *v1.Namespace, service *networkingv1.Ingress) *networkingv1.Ingress {
+	annotations := CopyAnnotations(service.Annotations)
+	annotations[constants.ManagedByAnnotationKey] = constants.ManagedByAnnotationValue
+
+	return &networkingv1.Ingress{}
 }
 
 func IsIngressManagedBy(ingress *networkingv1.Ingress) bool {
