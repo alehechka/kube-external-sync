270617e8a7d6cdb3825b169141198f922445b9f0
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-09-17 10:34:40 -0500

    regex functions

--- client/replicate/common/common.go
+++ client/replicate/common/common.go
@@ -1,9 +1,29 @@
 package common
 
-import v1 "k8s.io/api/core/v1"
+import (
+	"regexp"
+	"strings"
+
+	v1 "k8s.io/api/core/v1"
+)
 
 type Replicator interface {
 	Run()
 	Synced() bool
 	NamespaceAdded(ns *v1.Namespace)
+}
+
+func MatchStrictRegex(pattern, str string) (bool, error) {
+	return regexp.MatchString(BuildStrictRegex(pattern), str)
+}
+
+func BuildStrictRegex(regex string) string {
+	reg := strings.TrimSpace(regex)
+	if !strings.HasPrefix(reg, "^") {
+		reg = "^" + reg
+	}
+	if !strings.HasSuffix(reg, "$") {
+		reg = reg + "$"
+	}
+	return reg
 }
